dea30383fc0d4cb126cfb7c76cedf43cd2087e73
Kobi Anderson <Kanderson10222@gmail.com>
2018-05-14 15:14:00 -0400

    comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,14 +2,21 @@ class CommentsController < ApplicationController
 
   def create
     @post = Post.find(params[:post_id])
-    @comment = @post.user.comments.create(params[:comment].permit(:username, :content))
-    @comment.username = current_user.id
+    @comment = @post.comments.new(params[:comment].permit(:username, :content))
+    @comment.user_id = current_user.id
+    @comment.post_id = @post.id
     @comment.save
     redirect_to post_path(@post)
   end
 
   def destroy
+  @post = Post.find(params[:post_id])
+  @comment = @post.comments.find(params[:id])
+  if @comment.user_id == current_user.id
+  @comment.destroy
+end
+  redirect_to post_path(@post)
 
-  end
+end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_11_224530) do
+ActiveRecord::Schema.define(version: 2018_05_14_143138) do
 
   create_table "comments", force: :cascade do |t|
     t.string "username"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2018_05_11_224530) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.integer "post_id"
   end
 
   create_table "posts", force: :cascade do |t|
