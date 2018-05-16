22e495b7733b08a176eea736e4a7902ad63b2591
Kobi Anderson <Kanderson10222@gmail.com>
2018-05-16 15:04:52 -0400

    carousel implimented

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,15 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+var angle = 0;
+
+function galleryspin(sign) {
+  spinner = document.querySelector("#spinner");
+  if (!sign) {
+    angle = angle + 45;
+  } else {
+    angle = angle - 45;
+  }
+  spinner.setAttribute("style", "-webkit-transform: rotateY(" + angle + "deg); -moz-transform: rotateY(" + angle + "deg); transform: rotateY(" + angle + "deg);");
+}
