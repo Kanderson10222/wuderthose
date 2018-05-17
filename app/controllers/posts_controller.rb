ed9506810d0e0ce6d1c451e80204f652df8002ee
Kobi Anderson <Kanderson10222@gmail.com>
2018-05-17 17:55:44 -0400

    users

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,30 +1,24 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
-  # GET /posts
-  # GET /posts.json
+
+
   def index
     @posts = Post.all.order("created_at DESC")
     @post = Post.new
   end
 
-  # GET /posts/1
-  # GET /posts/1.json
   def show
     @post = Post.find(params[:id])
   end
 
-  # GET /posts/new
   def new
     @post = Post.new
   end
 
-  # GET /posts/1/edit
   def edit
   end
 
-  # POST /posts
-  # POST /posts.json
   def create
     @post = Post.new(post_params)
     @post.user_id = current_user.id
@@ -35,8 +29,6 @@ class PostsController < ApplicationController
         end
   end
 
-  # PATCH/PUT /posts/1
-  # PATCH/PUT /posts/1.json
   def update
     respond_to do |format|
       if @post.update(post_params)
@@ -49,8 +41,6 @@ class PostsController < ApplicationController
     end
   end
 
-  # DELETE /posts/1
-  # DELETE /posts/1.json
   def destroy
     @post.destroy
     respond_to do |format|
