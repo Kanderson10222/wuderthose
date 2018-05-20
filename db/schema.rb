9c99429e3e61945fc98f56896c0d0eb154be9cbe
Kobi Anderson <Kanderson10222@gmail.com>
2018-05-20 13:06:06 -0400

    avatar column added to post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_17_194636) do
+ActiveRecord::Schema.define(version: 2018_05_20_153954) do
 
   create_table "comments", force: :cascade do |t|
     t.string "username"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2018_05_17_194636) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.string "avatar"
   end
 
   create_table "users", force: :cascade do |t|
